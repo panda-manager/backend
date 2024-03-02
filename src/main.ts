5393d7d67166d72915baad0942ec3f8b6f5a9c8c
mayaraskin <mayaraskin14@gmail.com>
2024-03-03 00:46:34 +0200

    Added .env configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PORT } from './config/config';
 
-async function bootstrap() {
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+  await app.listen(PORT);
+};
+
+bootstrap().then(() => console.log(`Server is listening on port ${PORT}`));
