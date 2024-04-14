5ef22c89960f07ae13362fe57a46b5593ad36da2
mayaraskin <mayaraskin14@gmail.com>
2024-04-15 00:12:35 +0300

    Implemented Credentials DTOs

---
+++ src/modules/credentials/dto/credentials.dto.ts
@@ -0,0 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+
+export class CredentialsDTO {
+  @ApiProperty({ type: String, example: 'doesnt.matter.il' })
+  @IsNotEmpty()
+  readonly host: string;
+
+  @ApiProperty({ type: String, example: 'username' })
+  readonly username: string;
+}
---
+++ src/modules/credentials/dto/full_credentials.dto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+
+export class FullCredentialsDTO {
+  @ApiProperty({ type: String, example: 'doesnt.matter.il' })
+  @IsNotEmpty()
+  readonly host: string;
+
+  @ApiProperty({ type: String, example: 'username' })
+  readonly username: string;
+
+  @ApiProperty({ type: String })
+  readonly password: string;
+}
