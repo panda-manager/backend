62fa7dc6b643852f17d5cfdce20f9fafedd001e3
mayaraskin <mayaraskin14@gmail.com>
2024-04-15 00:32:36 +0300

    Implemented orm.config.ts

--- src/environments/index.ts
+++ src/environments/index.ts
@@ -1,4 +1,10 @@
-const PORT = parseInt(process.env.PORT) || 8080;
-const MONGO_URL = process.env.MONGO_URL as string;
+// App
+const APP_PORT = parseInt(process.env.PORT) || 8080;
+const NODE_ENV = process.env.NODE_ENV;
 
-export { PORT, MONGO_URL };
+// DB
+const MONGO_URL = process.env.MONGO_URL as string;
+const MONGO_PORT = parseInt(process.env.MONGO_PORT);
+const MONGO_DB = 'panda-manager';
+
+export { NODE_ENV, APP_PORT, MONGO_URL, MONGO_PORT, MONGO_DB };
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PORT } from './environments';
+import { APP_PORT } from './environments';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
+  await app.listen(APP_PORT);
 };
 
-bootstrap().then(() => console.log(`Server is listening on port ${PORT}`));
+bootstrap().then(() => console.log(`Server is listening on port ${APP_PORT}`));
