ad0287c22bbe6582760c8e9237891c786a115442
mayaraskin <mayaraskin14@gmail.com>
2024-04-16 01:31:10 +0300

    Added auth module & service skeletons

--- src/environments/index.ts
+++ src/environments/index.ts
@@ -8,4 +8,15 @@ const MONGO_URL = process.env.MONGO_URL as string;
 const MONGO_PORT = parseInt(process.env.MONGO_PORT);
 const MONGO_DB = 'panda-manager';
 
-export { NODE_ENV, APP_URL, APP_PORT, MONGO_URL, MONGO_PORT, MONGO_DB };
+// AUTH
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+
+export {
+  ACCESS_TOKEN_SECRET,
+  NODE_ENV,
+  APP_URL,
+  APP_PORT,
+  MONGO_URL,
+  MONGO_PORT,
+  MONGO_DB,
+};
