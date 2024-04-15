4f242d1da2332b56d41c63d78f41cd8aa229bdee
mayaraskin <mayaraskin14@gmail.com>
2024-04-15 21:31:01 +0300

    Added Swagger

--- src/environments/index.ts
+++ src/environments/index.ts
@@ -1,5 +1,6 @@
 // App
 const APP_PORT = parseInt(process.env.PORT) || 8080;
+const APP_URL = process.env.APP_URL || `http://localhost:${APP_PORT}`;
 const NODE_ENV = process.env.NODE_ENV;
 
 // DB
@@ -7,4 +8,4 @@ const MONGO_URL = process.env.MONGO_URL as string;
 const MONGO_PORT = parseInt(process.env.MONGO_PORT);
 const MONGO_DB = 'panda-manager';
 
-export { NODE_ENV, APP_PORT, MONGO_URL, MONGO_PORT, MONGO_DB };
+export { NODE_ENV, APP_URL, APP_PORT, MONGO_URL, MONGO_PORT, MONGO_DB };
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { APP_PORT } from './environments';
+import { SetupSwagger } from './utils/swagger/setup';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+  SetupSwagger(app);
   await app.listen(APP_PORT);
 };
 
