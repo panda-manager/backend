a5bcc0cf0984bb2b105bda05348d8d41ed3f761a
mayaraskin <mayaraskin14@gmail.com>
2024-05-06 18:24:00 +0300

    Implemented OTP module

---
+++ src/models/otp/entity/otp.entity.ts
@@ -0,0 +1,36 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  ObjectIdColumn,
+} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { ObjectId } from 'mongodb';
+import { Exclude } from 'class-transformer';
+
+@Entity({ name: 'otp' })
+export class OTPEntity {
+  @ApiProperty({ type: String, description: 'OTP ObjectId' })
+  @ObjectIdColumn()
+  readonly _id: ObjectId;
+
+  @ApiProperty({ type: ObjectId, description: "User's ObjectId" })
+  @Column()
+  @Exclude()
+  readonly user_id: ObjectId;
+
+  @ApiProperty({ type: String, description: 'OTP Code' })
+  @Column()
+  readonly otp: string;
+
+  @ApiProperty({ type: Date, description: 'Created at' })
+  @CreateDateColumn()
+  readonly created_at: Date;
+
+  constructor(partial: Partial<OTPEntity>) {
+    if (partial) {
+      Object.assign(this, partial);
+    }
+  }
+}
