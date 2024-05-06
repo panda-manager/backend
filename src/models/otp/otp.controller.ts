34fcba7d112744544c147807665e1b2ac314d22c
mayaraskin <mayaraskin14@gmail.com>
2024-05-06 19:54:42 +0300

    Reformatted

--- src/models/otp/otp.controller.ts
+++ src/models/otp/otp.controller.ts
@@ -13,7 +13,8 @@ import { OTPService } from './otp.service';
 @Controller('otp')
 export class OTPController {
   constructor(private readonly otp_service: OTPService) {}
-  @Put('/verify')
+
+  @Put('verify')
   async verify_otp(@Body() otp_verify_dto: OTPVerifyDTO) {
     await this.otp_service.verify_otp(otp_verify_dto);
 
