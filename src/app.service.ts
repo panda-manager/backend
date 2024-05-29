2413646c6f51fcf0b2c94acb72372bb6fd3b699c
mayaraskin <mayaraskin14@gmail.com>
2024-05-29 21:14:08 +0300

    Added files rewrite

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import * as fs from 'fs';
+import { NODE_ENV } from './environments';
 
 @Injectable()
-export class AppService {}
+export class AppService implements OnModuleInit {
+  onModuleInit() {
+    if (NODE_ENV !== 'production') return;
+
+    const cert_file_contents = fs.readFileSync('ssl/cert.pem').toString();
+    const key_file_contents = fs.readFileSync('ssl/key.pem').toString();
+
+    fs.writeFileSync(
+      'ssl/cert.pem',
+      cert_file_contents.replace(/\\n/g, '\n'),
+      'utf8',
+    );
+
+    fs.writeFileSync(
+      'ssl/key.pem',
+      key_file_contents.replace(/\\n/g, '\n'),
+      'utf8',
+    );
+  }
+}
