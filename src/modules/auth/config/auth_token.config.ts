638693cdafcbdeb0bc828bcceb24fd8f9c937e44
Maya Raskin <63534547+raskinmaya@users.noreply.github.com>
2024-08-16 13:56:28 +0300

    Added missing types (#40)

--- src/modules/auth/config/auth_token.config.ts
+++ src/modules/auth/config/auth_token.config.ts
@@ -1,10 +1,11 @@
 import { NODE_ENV } from '../../../environments';
+import { Mode } from '../../../common';
+import { JwtSignOptions } from '@nestjs/jwt';
 
-const authTokenConfig = {
-  development: {},
+const authTokenConfig: Partial<Record<Mode, Partial<JwtSignOptions>>> = {
   production: {
     expiresIn: '3000h',
   },
 };
 
-export default authTokenConfig[NODE_ENV];
+export default authTokenConfig[NODE_ENV] || {};
